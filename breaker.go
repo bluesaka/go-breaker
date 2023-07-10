397c2ad1f909f0755a4b7bd8a27cdf6eeee85bb1
bluesaka <real233@163.com>
2023-07-10 17:58:44 +0800

    update breaker

--- breaker.go
+++ breaker.go
@@ -52,87 +52,98 @@ func NewBreaker(opts ...Option) *Breaker {
 // Do do fn
 func (b *Breaker) Do(fn func() error) error {
 	// before call
-	if err := b.beforeCall(); err != nil {
+	batch, err := b.beforeCall()
+	if err != nil {
 		return err
 	}
 
 	// recover
 	defer func() {
 		if err := recover(); err != nil {
-			b.afterCall(false)
+			b.afterCall(batch, false)
 			//panic(err)
 		}
 	}()
 
 	// call function
-	err := fn()
+	err = fn()
 
 	// after call
-	b.afterCall(err == nil)
+	b.afterCall(batch, err == nil)
 
 	return err
 }
 
 // beforeCall before call
-func (b *Breaker) beforeCall() error {
+func (b *Breaker) beforeCall() (uint64, error) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
-	now := time.Now()
+
+	b.updateState(time.Now())
+
 	switch b.state {
 	case StateOpen:
-		// 过了冷却期，更新熔断器状态为半开
-		if b.openTime.Add(b.coolDownTime).Before(now) {
-			b.changeState(StateHalfOpen, now)
-			return nil
-		}
-		return ErrStateOpen
+		return b.metric.WindowBatch, ErrStateOpen
 	case StateHalfOpen:
 		// 请求数 ≥ 半开最大请求数，丢弃请求
 		if b.metric.TotalRequest >= b.halfOpenMaxCall {
-			return ErrStateHalfOpen
-		}
-	default:
-		if !b.metric.WindowStartTime.IsZero() && b.metric.WindowStartTime.Before(now) {
-			b.newWindow(now)
+			return b.metric.WindowBatch, ErrStateHalfOpen
 		}
 	}
-	return nil
+
+	b.metric.onRequest()
+	return b.metric.WindowBatch, nil
 }
 
 // after call
-func (b *Breaker) afterCall(result bool) {
+func (b *Breaker) afterCall(batch uint64, result bool) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
+
+	now := time.Now()
+	b.updateState(now)
+	if b.metric.WindowBatch != batch {
+		return
+	}
+
 	if result {
-		b.onSuccess(time.Now())
+		b.onSuccess(now)
 	} else {
-		b.onFail(time.Now())
+		b.onFail(now)
 	}
 }
 
 // newWindow create new window
 func (b *Breaker) newWindow(t time.Time) {
-	//b.metric.NewWindowBatch()
+	b.metric.NewWindowBatch()
 	b.metric.OnReset()
+
 	switch b.state {
 	case StateClosed:
-		if b.windowInterval == 0 {
-			b.metric.WindowStartTime = time.Now()
+		if b.windowInterval <= 0 {
+			b.metric.WindowExpiry = time.Time{}
 		} else {
-			b.metric.WindowStartTime = t.Add(b.windowInterval)
+			b.metric.WindowExpiry = t.Add(b.windowInterval)
 		}
 	case StateOpen:
-		b.metric.WindowStartTime = t.Add(b.coolDownTime)
+		b.metric.WindowExpiry = t.Add(b.coolDownTime)
 	default:
-		b.metric.WindowStartTime = time.Now()
+		b.metric.WindowExpiry = time.Time{}
 	}
 }
 
 // onSuccess call on success
 func (b *Breaker) onSuccess(t time.Time) {
 	b.metric.onSuccess()
-	if b.state == StateHalfOpen && b.metric.ContinuousSuccess >= b.halfOpenMaxCall {
-		b.changeState(StateClosed, t)
+	switch b.state {
+	case StateClosed:
+		if b.metric.WindowExpiry.Before(t) {
+			b.newWindow(t)
+		}
+	case StateHalfOpen:
+		if b.metric.ContinuousSuccess >= b.halfOpenMaxCall {
+			b.setState(StateClosed, t)
+		}
 	}
 }
 
@@ -142,21 +153,35 @@ func (b *Breaker) onFail(t time.Time) {
 	switch b.state {
 	case StateClosed:
 		if b.strategyFn(b.metric) {
-			b.changeState(StateOpen, t)
+			b.setState(StateOpen, t)
 		}
 	case StateHalfOpen:
-		b.changeState(StateOpen, t)
+		b.setState(StateOpen, t)
 	}
 }
 
-// changeState change breaker state
-func (b *Breaker) changeState(state State, t time.Time) {
+// setState set breaker state
+func (b *Breaker) setState(state State, t time.Time) {
 	if b.state == state {
 		return
 	}
+
 	b.state = state
+	b.openTime = t
 	b.newWindow(t)
-	if state == StateOpen {
-		b.openTime = t
+}
+
+// updateState update state
+func (b *Breaker) updateState(t time.Time) {
+	switch b.state {
+	case StateClosed:
+		if !b.metric.WindowExpiry.IsZero() && b.metric.WindowExpiry.Before(t) {
+			b.newWindow(t)
+		}
+	case StateOpen:
+		if b.metric.WindowExpiry.Before(t) {
+			b.newWindow(t)
+			b.setState(StateHalfOpen, t)
+		}
 	}
 }
--- breaker_test.go
+++ breaker_test.go
@@ -14,7 +14,7 @@ import (
 func TestBreaker(t *testing.T) {
 	strategyFailOpt := StrategyOption{
 		Strategy:      StrategyFail,
-		FailThreshold: 2,
+		FailThreshold: 3,
 	}
 	//strategyContinuousFailOpt := StrategyOption{
 	//	Strategy:                StrategyContinuousFail,
@@ -26,20 +26,30 @@ func TestBreaker(t *testing.T) {
 	//	MinCall:  10,
 	//}
 
-	breaker := NewBreaker(WithName("breakerName"), WithWindowInterval(time.Second), WithCoolDownTime(time.Second),
-		WithHalfOpenMaxCall(2), WithStrategyOption(strategyFailOpt))
+	breaker := NewBreaker(
+		WithName("breakerName"),
+		WithWindowInterval(10*time.Second),
+		WithCoolDownTime(2*time.Second),
+		WithHalfOpenMaxCall(2),
+		WithStrategyOption(strategyFailOpt),
+	)
 
-	for i := 0; i < 20; i++ {
+	for i := 1; i <= 20; i++ {
 		log.Println("i:", i)
-		breaker.Do(func() error {
+		err := breaker.Do(func() error {
 			rand.Seed(time.Now().UnixNano())
-			if rand.Intn(2) == 1 {
-				return nil
+			if i == 1 || i == 2 || i == 3 || i == 6 || i == 11 {
+				return errors.New("")
 			} else {
-				return errors.New("error")
+				return nil
 			}
 		})
+		if errors.Is(err, ErrStateHalfOpen) || errors.Is(err, ErrStateOpen) {
+			log.Printf("err: %s", err.Error())
+		}
+
 		fmt.Println()
-		time.Sleep(time.Millisecond * 500)
+		time.Sleep(time.Second)
 	}
+
 }
